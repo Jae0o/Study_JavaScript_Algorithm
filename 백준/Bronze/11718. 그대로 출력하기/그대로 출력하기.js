23c37f6a3cb5919a0f508c1b7f2b405c445d1477
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-10-18 19:59:00 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 136 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n')
+input.forEach(item => console.log(item))
+
