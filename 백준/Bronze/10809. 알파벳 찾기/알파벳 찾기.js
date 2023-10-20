9a8ed4deb88c45213e720db266e31c23e6a6f927
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-10-20 20:51:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 140 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n')
+input = input[0]
+
+const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const result = arr.map(alpha => input.includes(alpha) ? input.indexOf(alpha) : -1)
+console.log(result.join(" "))
