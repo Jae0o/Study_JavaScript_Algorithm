8edc3d45bcaa8eb00de74a83b6e734f758cd9fa5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-08 00:04:45 +0900

    [Silver IV] Title: 스택 2, Time: 2068 ms, Memory: 252452 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.js
@@ -0,0 +1,42 @@
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+const inputs = [];
+
+rl.on("line", (input) => {
+  inputs.push(input);
+}).on("close", () => {
+  const N = inputs.shift();
+  const arr = inputs.map((item) => item.split(" "));
+
+  solution(arr);
+});
+
+function solution(arr) {
+  const stack = [];
+  const result = [];
+
+  for (const order of arr) {
+    if (order[0] === "1") {
+      stack.push(order[1]);
+    }
+
+    if (order[0] === "2") {
+      const value = stack.pop();
+      value ? result.push(value) : result.push("-1");
+    }
+
+    if (order[0] === "3") {
+      result.push(stack.length);
+    }
+
+    if (order[0] === "4") {
+      stack.length ? result.push("0") : result.push("1");
+    }
+
+    if (order[0] === "5") {
+      const value = stack.length ? stack[stack.length - 1] : -1;
+      result.push(value);
+    }
+  }
+
+  console.log(result.join("\n"));
+}
