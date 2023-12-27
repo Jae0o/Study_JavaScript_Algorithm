896a0cd58d047378f3e6d3d32e3c87e82925e5d8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-27 16:57:49 +0900

    [Bronze V] Title: ??!, Time: 156 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,8 @@
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+let inputs = [];
+
+rl.on("line", (input) => {
+  inputs.push(input);
+}).on("close", () => {
+  console.log(inputs[0] + "??!");
+});
