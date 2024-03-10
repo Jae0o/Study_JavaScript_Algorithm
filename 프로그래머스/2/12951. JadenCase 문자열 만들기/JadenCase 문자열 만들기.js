56d463865384d7a4e85260b4baef4bcf84a31a4c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-10 23:20:21 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,4 @@
 function solution(s) {
-
     return s.toLowerCase().split(" ").map(i => i.replace(/\w/g,(item,index)=> 
                                            index === 0 ? item.toUpperCase() :item)).join(" ")
-    
 }
