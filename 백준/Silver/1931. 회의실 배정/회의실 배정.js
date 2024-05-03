35dfe0b0d28875a6fd94bd5d7466246bbf12d8f0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-03 14:29:35 +0900

    [Silver I] Title: 회의실 배정, Time: 572 ms, Memory: 42420 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -4,29 +4,31 @@ let inputs = [];
 rl.on("line", (input) => {
   inputs.push(input);
 }).on("close", () => {
-  const N = inputs.shift();
-  const arr = inputs
-    .map((item) => item.split(" "))
-    .sort((a, b) => {
-      if (a[1] === b[1]) {
-        return parseInt(a[0]) - parseInt(b[0]);
-      }
+  const N = +inputs.shift();
 
-      return parseInt(a[1]) - parseInt(b[1]);
-    });
-  solution(arr);
+  solution(
+    N,
+    inputs.map((item) => item.split(" ").map(Number))
+  );
 });
 
-function solution(arr) {
-  let time = 0;
-  let count = 0;
+function solution(N, list) {
+  const sorted = list.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    }
+    return a[1] - b[1];
+  });
 
-  for (const [start, end] of arr) {
-    if (parseInt(start, 10) >= time) {
-      time = parseInt(end, 10);
+  let endTime = sorted[0][1];
+  let count = 1;
+
+  for (let i = 1; i < N; i++) {
+    const [start, end] = sorted[i];
+
+    if (start >= endTime) {
+      endTime = end;
       count++;
-
-      continue;
     }
   }
 
