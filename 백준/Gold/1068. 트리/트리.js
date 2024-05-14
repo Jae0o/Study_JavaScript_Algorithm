d5515c0532c0a90e254626158fbe163e3d439f3f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-14 12:15:25 +0900

    [Gold V] Title: 트리, Time: 160 ms, Memory: 9500 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.js
@@ -0,0 +1,64 @@
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+let inputs = [];
+
+rl.on("line", (input) => {
+  inputs.push(input);
+}).on("close", () => {
+  const N = inputs.shift();
+  const list = inputs.shift().split(" ").map(Number);
+  const remove = inputs.shift();
+
+  solution(+N, list, +remove);
+});
+
+function solution(N, list, remove) {
+  let rootNumber = 0;
+  const graph = {};
+
+  for (let i = 0; i < N; i++) {
+    graph[i] = [];
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (list[i] === -1) {
+      rootNumber = i;
+      continue;
+    }
+
+    if (i === remove || list[i] === remove) {
+      continue;
+    }
+
+    graph[list[i]].push(i);
+  }
+
+  if (rootNumber === remove) {
+    return console.log(0);
+  }
+
+  graph[remove] = [];
+
+  const visited = new Array(N).fill(false);
+  // visited[rootNumber] = true;
+
+  const stack = [rootNumber];
+
+  let result = 0;
+
+  while (stack.length) {
+    const index = stack[stack.length - 1];
+
+    if (graph[index].length) {
+      visited[index] = true;
+      stack.push(graph[index].pop());
+    } else {
+      stack.pop();
+
+      if (!visited[index]) {
+        result++;
+      }
+    }
+  }
+
+  console.log(result);
+}
