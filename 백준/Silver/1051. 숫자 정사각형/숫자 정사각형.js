93e73bfa8856cfa140eaaf7823c4463bee684208
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-05 14:05:36 +0900

    [Silver III] Title: 숫자 정사각형, Time: 212 ms, Memory: 10216 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.js
@@ -0,0 +1,40 @@
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+let inputs = [];
+
+rl.on("line", (input) => {
+  inputs.push(input);
+}).on("close", () => {
+  const [N, M] = inputs.shift().split(" ").map(Number);
+
+  solution(N, M, inputs);
+});
+
+function solution(N, M, list) {
+  const graph = list.map((item) => item.split(""));
+
+  let range = 1;
+
+  for (let n = 0; n < N; n++) {
+    for (let m = 0; m < M; m++) {
+      const current = graph[n][m];
+
+      for (let i = m + 1; i < M; i++) {
+        const distance = i - m;
+
+        if (n + distance >= N) {
+          continue;
+        }
+
+        if (
+          current === graph[n][distance + m] &&
+          current === graph[distance + n][m] &&
+          current === graph[distance + n][distance + m]
+        ) {
+          range = Math.max(range, distance + 1);
+        }
+      }
+    }
+  }
+
+  console.log(range * range);
+}
