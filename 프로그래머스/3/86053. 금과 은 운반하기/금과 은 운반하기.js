e607b612d04be2d6f80a20af97776adaf4e602ea
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-05 18:39:35 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.js
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.js
@@ -1,4 +1,4 @@
-function solution(a, b, g, s, w, t) {
+const solution = (a, b, g, s, w, t) => {
   let start = 0;
   // 제한 사항에 맞도록 최대 값을 설정
   let end = 10e9 * 2 * 10e5 * 2;
@@ -7,16 +7,22 @@ function solution(a, b, g, s, w, t) {
   const totalVillage = t.length;
 
   while (start <= end) {
+    // 해당 루프에 담을 전체 금
     let golds = 0;
+    // 해당 루프에 담을 전체 은
     let silvers = 0;
+    // 전체 자원 량
     let resource = 0;
 
     for (let i = 0; i < totalVillage; i++) {
+      // 해당 마을의 옮길수 있는 금 용량
       const villageGold = g[i];
+      // 해당 마을의 옮길 수 있는 은 용량
       const villageSilver = s[i];
-
+      // 왕복 가능한 횟수
       const move = Math.round(mid / (2 * t[i])) * w[i];
 
+      // 옮길 수 잇는 금 은 전체 광물 양을 넣음
       golds += Math.min(villageGold, move);
       silvers += Math.min(villageSilver, move);
       resource += Math.min(villageGold + villageSilver, move);
@@ -32,4 +38,4 @@ function solution(a, b, g, s, w, t) {
   }
 
   return start;
-}
+};
